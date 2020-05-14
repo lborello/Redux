215931799cda7b21c94f199b6a6fd402280514cd
Luis Borello <luis_borello@hotmail.com>
2020-05-14 17:53:20 -0300

    Seccion 7 terminada
    Seccion 7 terminada

---
+++ 04-ingreso-egreso/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAF9_x3BK7Xp1YIHn7nQGHZsxeJDrG_Fhg',
+    authDomain: 'ingreso-egreso-app-8910b.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-8910b.firebaseio.com',
+    projectId: 'ingreso-egreso-app-8910b',
+    storageBucket: 'ingreso-egreso-app-8910b.appspot.com',
+    messagingSenderId: '783664347393',
+    appId: '1:783664347393:web:23426348f4f98ab3614300'
+  }
+};
---
+++ redux-ingreso-egreso-1.1.0/src/app/auth/register/register.component.ts
@@ -0,0 +1,67 @@
+import {
+  Component,
+  OnInit
+} from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styles: []
+})
+export class RegisterComponent implements OnInit {
+
+  registroForm: FormGroup;
+
+  constructor( private fb: FormBuilder,
+               private authService: AuthService,
+               private router: Router) { }
+
+  ngOnInit() {
+
+    this.registroForm = this.fb.group({
+      nombre:   ['', Validators.required ],
+      correo:   ['', [Validators.required, Validators.email ] ],
+      password: ['', Validators.required ],
+    });
+
+  }
+
+  crearUsuario() {
+
+    if ( this.registroForm.invalid ) { return; }
+    
+    // Swal.fire({
+    //   title: 'Espere por favor',
+    //   onBeforeOpen: () => {
+    //     Swal.showLoading()
+    //   }
+    // });
+
+
+    const { nombre, correo, password } = this.registroForm.value;
+    console.log(nombre);
+
+  //   this.authService.crearUsuario( nombre, correo, password )
+  //     .then( (credenciales: any) => {
+  //       console.log(credenciales);
+  //       Swal.close();
+  //       this.router.navigate(['/']);
+  //     })
+  //     .catch( err => {
+  //       Swal.fire({
+  //         icon: 'error',
+  //         title: 'Oops...',
+  //         text: err.message
+  //       })
+  //     });
+  // }
+
+}
